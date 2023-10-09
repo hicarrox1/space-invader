4174d4e7377d17688e3171aa60f4cde6d66a812a
hicarrox1 <hicarrox@gmail.com>
2023-10-09 12:00:52 +0200

    commit 1

---
+++ main.py
@@ -0,0 +1,14 @@
+class Vaisseau():
+
+    def __init__(self) -> None:
+        pass
+
+class App():
+
+    def __init__(self) -> None:
+        
+        self.score = 0
+        self.vaisseau = Vaisseau()
+
+
+App()
